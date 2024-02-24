e3a750c645233539e8d48941b01d19589f5c66bf
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-02-24 03:25:14 +0000

    test

--- main.cs
+++ main.cs
@@ -2,6 +2,20 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    
+    string Wrd;
+    string PrevWrd = "";
+    do
+    {
+      Console.WriteLine("Enter something: ");
+      Wrd = Console.ReadLine();
+      if (Wrd == "exit" || Wrd == "Exit") 
+      break;
+
+      PrevWrd = PrevWrd + " " + Wrd;
+      Console.WriteLine(PrevWrd.Trim());
+      } 
+      while (Wrd != "exit" && Wrd != "Exit");
+    Console.WriteLine(" ");
+    Console.WriteLine("You have exited the program");
   }
 }
